cd0790e3d59305c0e27192ddd1d727199686ed65
Frederic LE BLAY <fle-blay@e2r1p3.clusters.42paris.fr>
2022-05-20 10:12:30 +0200

    Fix Megaphone and month display for account (ex02)

--- C00/ex00/src/megaphone.cpp
+++ C00/ex00/src/megaphone.cpp
@@ -6,20 +6,29 @@
 /*   By: fle-blay <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/18 14:47:19 by fle-blay          #+#    #+#             */
-/*   Updated: 2022/05/18 15:06:33 by fle-blay         ###   ########.fr       */
+/*   Updated: 2022/05/20 10:04:22 by fle-blay         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <locale>
+#include <string>
 
 int	main(int ac, char *av[])
 {
+	std::locale	loc;
+	std::string	str;
+
 	if (ac == 1)
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 	else
 	{
 		while (*++av)
-			std::cout << *av;
+		{
+			str = *av;
+			for (std::string::size_type i = 0; i < str.length(); i++)
+				std::cout << std::toupper(str[i], loc);
+		}
 		std::cout << std::endl;
 	}
 	return (0);
--- C00/ex02/src/Account.cpp
+++ C00/ex02/src/Account.cpp
@@ -134,7 +134,7 @@ void Account::_displayTimestamp(void)
 	std::cout	<< "["
 				<< local_time->tm_year + 1900
 				<< std::setfill('0') << std::setw(2)
-				<< local_time->tm_mon
+				<< local_time->tm_mon + 1
 				<< std::setfill('0') << std::setw(2)
 				<< local_time->tm_mday
 				<< "_"

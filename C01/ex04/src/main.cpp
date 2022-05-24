fddb2d9a038bd8f25ff30383d628b1dad649bcef
Frederic LE BLAY <fle-blay@e2r2p7.clusters.42paris.fr>
2022-05-24 10:29:38 +0200

    Ajout fin ex04

--- C01/ex04/src/main.cpp
+++ C01/ex04/src/main.cpp
@@ -7,12 +7,10 @@ int	main(int ac, char *av[])
 {
 	std::string			arg[3];
 	std::ifstream		ifs;
+	std::ofstream		ofs;
 	std::stringstream	buf;
-	std::string			outfile;
-	std::string			input;
 	std::size_t			pos_next_to_replace;
 	std::size_t			offset = 0;
-	std::size_t			count = 0;
 
 	if (ac != 4)
 	{
@@ -28,7 +26,6 @@ int	main(int ac, char *av[])
 			return (1);
 		}
 	}
-	outfile = arg[0] + ".replace";
 	ifs.open(arg[0].c_str(), std::ifstream::in);
 	if (!ifs.good())
 	{
@@ -43,21 +40,14 @@ int	main(int ac, char *av[])
 		std::cout	<< "Error loading file into temporary string" << std::endl;
 		return (1);
 	}
-	//to access data
-	std::cout << arg[1] << std::endl;
 	while ((pos_next_to_replace = buf.str().find(arg[1], offset)) != std::string::npos)
 	{
-		count++;
-		std::cout << "next post : " << pos_next_to_replace << std::endl;
-		/*
-		buf.str().erase(pos_next_to_replace, arg[1].size());
-		buf.str().insert(pos_next_to_replace, arg[2]);
+		buf.str(buf.str().erase(pos_next_to_replace, arg[1].size()));
+		buf.str(buf.str().insert(pos_next_to_replace, arg[2]));
 		offset = pos_next_to_replace + arg[2].size();
-		*/
-		offset = pos_next_to_replace + arg[1].size();
-		std::cout << "offset :" << offset << std::endl;
 	}
-	std::cout << count << std::endl;
-	std::cout << buf.str();
+	ofs.open(arg[0].append(".replace").c_str(), std::ofstream::out);
+	ofs << buf.str();
+	ofs.close();
 	return (0);
 }

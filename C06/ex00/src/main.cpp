8bc33cc883c1198a39fd4494b5705a93dd098845
Frederic LE BLAY <fle-blay@e1r13p1.42.fr>
2022-06-09 12:26:48 +0200

    work in progress CPP06

--- C06/ex00/src/main.cpp
+++ C06/ex00/src/main.cpp
@@ -1,12 +1,78 @@
 #include <iostream>
 #include <cstdlib>
+#include <cctype>
 #include <string>
 
+# define ERROR 0
+# define CHAR 1
+# define FLOAT 2
+# define DOUBLE 3
+# define INT 4
 
-//Dectect right type with parsing
-//convert using strtod etc...
-//Use static cast to create other value
-//Display
+int	get_type(char *arg)
+{
+	std::string	input = arg;
+	std::size_t	f_position = input.find_last_of("f");
+	std::size_t	dot_position = input.find_last_of(".");
+	std::size_t	minus_position = input.find_last_of("-");
+	std::size_t	arg_len = input.length();
+	std::size_t	f_position_error = (input.find_first_of("f") != input.find_last_of("f"));
+	std::size_t	dot_position_error = input.find_first_of(".") != input.find_last_of(".");
+	std::size_t	minus_position_error = input.find_first_of("-") != input.find_last_of("-");
+	std::size_t min_correct_len = 1
+								+ (minus_position != std::string::npos)
+								+ (dot_position != std::string::npos)
+								+ (f_position != std::string::npos);
+	if (arg_len == 0)
+		return (std::cout << "Input is empty!" << std::endl, ERROR);
+	for (std::size_t i = 0; i < arg_len; i++)
+	{
+		if (!isprint(input[i]))
+			return (std::cout << "There is a non-printable char!" << std::endl, ERROR);
+	}
+	//char road
+	if (!isdigit(input[0]) && arg_len == 1)
+	{
+		std::cout << "\x1b[31marg is char\x1b[0m" << std::endl;
+		return (CHAR);
+	}
+	//number road
+	if (input[0] == '-' && arg_len > 1 && !isdigit(input[1]) && input[1] != '.')
+			return (std::cout << "Wrong negative number format!" << std::endl, ERROR);
+	if (input.find_first_not_of("0123456789-f.") != std::string::npos)
+		return (std::cout << "Wrong char among number format!" << std::endl, ERROR);
+	if (f_position != std::string::npos && f_position != arg_len - 1)
+		return (std::cout << "Wrong float sign position!" << std::endl, ERROR);
+	if (f_position_error)
+		return (std::cout << "Too many float signs!" << std::endl, ERROR);
+	if (dot_position_error)
+		return (std::cout << "Too many dot signs!" << std::endl, ERROR);
+	if (minus_position_error)
+		return (std::cout << "Too many minus signs!" << std::endl, ERROR);
+	if (minus_position != std::string::npos && minus_position != 0)
+		return (std::cout << "Wrong minus sign position!" << std::endl, ERROR);
+	if (f_position != std::string::npos && f_position != arg_len - 1)
+		return (std::cout << "Wrong float sign position!" << std::endl, ERROR);
+	if (arg_len < min_correct_len)
+		return (std::cout << "Missing value!" << std::endl, ERROR);
+
+	//return number type
+	if (f_position != std::string::npos || input == "-inff" || input == "+inff" || input == "nanf")
+	{
+		std::cout << "\x1b[32marg is a float\x1b[0m" << std::endl;
+		return (FLOAT);
+	}
+	else if (dot_position != std::string::npos || input == "-inf" || input == "+inf" || input == "nan")
+	{
+		std::cout << "\x1b[33marg is a double\x1b[0m" << std::endl;
+		return (DOUBLE);
+	}
+	else
+	{
+		std::cout << "\x1b[34marg is a int\x1b[0m" << std::endl;
+		return (INT);
+	}
+}
 
 int	main(int ac, char *av[])
 {
@@ -16,70 +82,10 @@ int	main(int ac, char *av[])
 		return (1);
 	}
 
-	std::string	input = av[1];
-
-	std::size_t	f_position = input.find_last_of("f");
-	std::size_t	dot_position = input.find_last_of(".");
-	std::size_t	minus_position = input.find_last_of("-");
-
-	std::size_t	arg_len = input.length();
-
-	std::size_t	f_position_error = input.find_first_of("f") != input.find_last_of("f");
-	std::size_t	dot_position_error = input.find_first_of(".") != input.find_last_of(".");
-	std::size_t	minus_position_error = input.find_first_of("-") != input.find_last_of("-");
-
-	std::size_t min_correct_len = 1
-								+ (minus_position != std::string::npos)
-								+ (dot_position != std::string::npos)
-								+ (f_position != std::string::npos);
-	//fx to check que des char ou les valeurs speciales des floats ou que des digits	
-	if (arg_len == 0)
-	{
-		std::cout << "Input is empty!" << std::endl;
+	int	type = get_type(av[1]);
+	if (type == 0)
 		return (1);
-	}
-	if (f_position_error)
-	{
-		std::cout << "Too many float signs!" << std::endl;
-		return (1);
-	}
-	if (dot_position_error)
-	{
-		std::cout << "Too many dot signs!" << std::endl;
-		return (1);
-	}
-	if (minus_position_error)
-	{
-		std::cout << "Too many minus signs!" << std::endl;
-		return (1);
-	}
-	if (minus_position != std::string::npos && minus_position != 0)
-	{
-		std::cout << "Wrong minus sign position!" << std::endl;
-		return (1);
-	}
-	if (f_position != std::string::npos && f_position != arg_len - 1)
-	{
-		std::cout << "Wrong float sign position!" << std::endl;
-		return (1);
-	}
-	if (arg_len < min_correct_len)
-	{
-		std::cout << "Missing value!" << std::endl;
-		return (1);
-	}
-	std::cout	<< "f_position : " << f_position << std::endl
-				<< "dot_position : " << dot_position << std::endl
-				<< "arg_len : " << arg_len << std::endl
-				<< "min_correct_len : " << min_correct_len << std::endl;
-
-	if (f_position == arg_len - 1
-		&& ((dot_position < f_position && arg_len > 2)
-		|| (dot_position == std::string::npos && arg_len > 1)))
-	{
-		std::cout << "arg is a float" << std::endl;
-	}
-
-	std::cout << -4.f << std::endl;
+	else
+		std::cout << "make conversion" << std::endl;
 	return (0);
 }

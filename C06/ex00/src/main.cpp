4764d2a29abca122025290472d8dc42a91035478
Frederic LE BLAY <fle-blay@e1r13p1.42.fr>
2022-06-09 13:06:22 +0200

    work in progress Day 06 cpp exo 1

--- C06/ex00/src/main.cpp
+++ C06/ex00/src/main.cpp
@@ -15,12 +15,14 @@ int	get_type(char *arg)
 	std::size_t	f_position = input.find_last_of("f");
 	std::size_t	dot_position = input.find_last_of(".");
 	std::size_t	minus_position = input.find_last_of("-");
+	std::size_t	plus_position = input.find_last_of("+");
 	std::size_t	arg_len = input.length();
 	std::size_t	f_position_error = (input.find_first_of("f") != input.find_last_of("f"));
 	std::size_t	dot_position_error = input.find_first_of(".") != input.find_last_of(".");
 	std::size_t	minus_position_error = input.find_first_of("-") != input.find_last_of("-");
+	std::size_t	plus_position_error = input.find_first_of("+") != input.find_last_of("+");
 	std::size_t min_correct_len = 1
-								+ (minus_position != std::string::npos)
+								+ ((minus_position != std::string::npos) || plus_position != std::string::npos)
 								+ (dot_position != std::string::npos)
 								+ (f_position != std::string::npos);
 	if (arg_len == 0)
@@ -36,23 +38,40 @@ int	get_type(char *arg)
 		std::cout << "\x1b[31marg is char\x1b[0m" << std::endl;
 		return (CHAR);
 	}
+
+	//exception road
+	if (input == "-inff" || input == "+inff" || input == "nanf")
+	{
+		std::cout << "\x1b[32marg is a float\x1b[0m" << std::endl;
+		return (FLOAT);
+	}
+	if (input == "-inf" || input == "+inf" || input == "nan")
+	{
+		std::cout << "\x1b[33marg is a double\x1b[0m" << std::endl;
+		return (DOUBLE);
+	}
+
 	//number road
 	if (input[0] == '-' && arg_len > 1 && !isdigit(input[1]) && input[1] != '.')
 			return (std::cout << "Wrong negative number format!" << std::endl, ERROR);
-	if (input.find_first_not_of("0123456789-f.") != std::string::npos)
+	if (input.find_first_not_of("0123456789-+f.") != std::string::npos)
 		return (std::cout << "Wrong char among number format!" << std::endl, ERROR);
 	if (f_position != std::string::npos && f_position != arg_len - 1)
 		return (std::cout << "Wrong float sign position!" << std::endl, ERROR);
+	if (minus_position != std::string::npos && plus_position != std::string::npos)
+		return (std::cout << "Too many signs : plus and minus signs!" << std::endl, ERROR);
 	if (f_position_error)
 		return (std::cout << "Too many float signs!" << std::endl, ERROR);
 	if (dot_position_error)
 		return (std::cout << "Too many dot signs!" << std::endl, ERROR);
 	if (minus_position_error)
 		return (std::cout << "Too many minus signs!" << std::endl, ERROR);
+	if (plus_position_error)
+		return (std::cout << "Too many plus signs!" << std::endl, ERROR);
 	if (minus_position != std::string::npos && minus_position != 0)
 		return (std::cout << "Wrong minus sign position!" << std::endl, ERROR);
-	if (f_position != std::string::npos && f_position != arg_len - 1)
-		return (std::cout << "Wrong float sign position!" << std::endl, ERROR);
+	if (plus_position != std::string::npos && plus_position != 0)
+		return (std::cout << "Wrong plus sign position!" << std::endl, ERROR);
 	if (arg_len < min_correct_len)
 		return (std::cout << "Missing value!" << std::endl, ERROR);
 
@@ -74,6 +93,26 @@ int	get_type(char *arg)
 	}
 }
 
+void	display(int type, char *arg)
+{
+	char	Char = 0;
+	int		Int = 0;
+	float	Float = 0;
+	double	Double = 0;
+
+	if (type == FLOAT)
+	{
+		Float = strtof(arg, NULL);
+		Char = static_cast<char>(Float);
+		Double = static_cast<double>(Float);
+		Int = static_cast<int>(Float);
+	}
+	std::cout	<< "char: '" << Char << "'" << std::endl
+				<< "int: " << Int << std::endl
+				<< "float: " << Float << std::endl
+				<< "double: " << Double << std::endl;
+}
+
 int	main(int ac, char *av[])
 {
 	if (ac != 2)
@@ -81,11 +120,13 @@ int	main(int ac, char *av[])
 		std::cout << "Wrong arg count!" << std::endl;
 		return (1);
 	}
-
 	int	type = get_type(av[1]);
-	if (type == 0)
+	if (type == ERROR)
 		return (1);
 	else
+	{
 		std::cout << "make conversion" << std::endl;
+		display(type, av[1]);
+	}
 	return (0);
 }
